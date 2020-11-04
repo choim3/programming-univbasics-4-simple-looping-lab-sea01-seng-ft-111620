ed7c18f486daf47682b42314cb731822e4a31d17
gentle-expression-4880 <choim3@spu.edu>
2020-11-04 05:28:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,3 +21,13 @@ def output_array(tacos)
     counter += 1;
   end
 end
+
+def return_string_array(array)
+  counter = 0;
+  while counter < array.length do
+  array[counter] = array[counter].to_s 
+  counter += 1;
+end 
+return array
+end
+  puts 
